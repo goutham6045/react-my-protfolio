2ca325929490d5bf0e6491a433fc3e8158ed6ade
goutham6045 <goutham6045@.com>
2023-08-19 19:39:29 -0400

    portfolio home page bug resolved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroImage from '../assets/heroImage.png';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import {Link} from "react-scroll";
 
 const Home = () => {
   return (
@@ -20,7 +21,10 @@ const Home = () => {
                 </p>
 
                 <div>
-                    <button className="group text-white w-fit px-6 py-3 my-2 
+                    <Link to="portfolio" 
+                    smooth
+                    duration={500}
+                    className="group text-white w-fit px-6 py-3 my-2 
                     flex items-center rounded-md bg-gradient-to-r
                      from-cyan-500 to-blue-500 cursor-pointer ">
                         Portfolio
@@ -28,7 +32,7 @@ const Home = () => {
                             <MdOutlineKeyboardArrowRight size={25}
                             className="ml-l"/>
                         </span>
-                    </button>
+                    </Link>
                 </div>
             </div>
 
